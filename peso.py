16d805fad31d0428d82f7f894bdbcb522393c6ba
Estado Nacion <estadonacion@MacBook-Pro-de-Estado.local>
2018-06-02 17:10:23 -0600

    Remover peso

--- peso.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-weight = float(input("Cuánto mides? (m)\n")) 
-if weight > 2:
-	print("wow! eres realmente alto/a")
-print("Mides " + str(weight) + " metros.\nGracias por participar!")
