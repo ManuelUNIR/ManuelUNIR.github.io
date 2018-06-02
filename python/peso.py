17609463bd855e7723aed687e1e8a5a3ee529e4d
Estado Nacion <estadonacion@MacBook-Pro-de-Estado.local>
2018-06-02 16:43:20 -0600

    Archivos clase Python

---
+++ python/peso.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+weight = float(input("Cuánto mides? (m)\n")) 
+if weight > 2:
+	print("wow! eres realmente alto/a")
+print("Mides " + str(weight) + " metros.\nGracias por participar!")
